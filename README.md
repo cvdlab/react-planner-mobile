a1a7e77e41a482280d42e156da6c26f13e306cf9
alittera <consolelite@gmail.com>
2016-12-06 14:27:42 +0100

    README.md modified

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # react-planner-mobile
 
 ### Mobile porting of [https://github.com/cvdlab/react-planner](https://github.com/cvdlab/react-planner)
+
+## Let's start
+```
+  npm install && npm start
+```
--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -3,7 +3,6 @@ import React, {PropTypes} from 'react';
 
 export default function Comments({points}) {
 
-    //<circle fill='#00a' r='42' cx='" + point[i] + "' cy='" + point[i] + "'>
 
     let jsx = points.map((coords,i) => <circle key={i} fill='#00a' r='42' cx={coords.get('x')} cy={coords.get('y')}/>);
 
