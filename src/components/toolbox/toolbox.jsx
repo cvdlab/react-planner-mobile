58528eebb4dcf6657cdce1115f9616344eb0dd60
alittera <consolelite@gmail.com>
2016-12-16 17:57:42 +0100

    Scroll disabled on mobile

--- src/components/toolbox/toolbox.jsx
+++ src/components/toolbox/toolbox.jsx
@@ -12,9 +12,9 @@ import ToolboxButton from './toolbox-button';
 const STYLE = {
     
     position: 'absolute',
-    bottom: '10px',
+    bottom: '0px',
     right: '0px',
-    padding: "10px 10px 0px 10px"
+    padding: "10px 10px 10px 10px"
 
 };
 
