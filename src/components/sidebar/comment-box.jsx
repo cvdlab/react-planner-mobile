ce76c42b6f13efffd2c0d9bd3af272151a2297ee
giuliom95 <giuliomartella1995@gmail.com>
2016-12-19 16:00:23 +0100

    Added simple comment-box style

--- src/components/sidebar/comment-box.jsx
+++ src/components/sidebar/comment-box.jsx
@@ -1,10 +1,25 @@
 import React, {PropTypes} from 'react';
 
-const STYLE = {};
+const STYLE = {
+    background: "#478dda",
+    padding: 20,
+    margin: 5,
+    color: "white",
+    fontFamily: "helvetica"
+};
 
 
-export default function CommentBox() {
+export default function CommentBox({state, index}) {
 
-    return ();
+    return (
+        <div style={{...STYLE}}>
+            {state.comments.get(index)}
+        </div>
+    )
 
 }
+
+Toolbox.propTypes = {
+    state: PropTypes.object.isRequired,
+    index: PropTypes.number.isRequired
+};
