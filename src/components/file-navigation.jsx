24f0bc2ddc625a16bc10ac1b9f00df367397aec7
alittera <consolelite@gmail.com>
2017-01-19 13:35:31 +0100

    works on file navigation

--- src/components/file-navigation.jsx
+++ src/components/file-navigation.jsx
@@ -1,9 +1,6 @@
 import React, {PropTypes} from 'react';
 
-import IconCheck from 'react-icons/lib/fa/check';
 import IconClose from 'react-icons/lib/fa/close';
-import IconFile from 'react-icons/lib/fa/file-text-o';
-import IconFolder from 'react-icons/lib/fa/folder-o';
 
 
 const STYLE = {
@@ -45,6 +42,7 @@ const STYLE_BUTTON = {
 
 const STYLE_UL = {
     width: "100%",
+    minWidth: "400px",
     height: "220px",
     color: "black",
     fontFamily: "helvetica",
@@ -54,106 +52,38 @@ const STYLE_UL = {
     backgroundColor: "#fbfbfb",
     padding: "20px 20px 20px 20px",
     maxWidth: "600px",
-    overflow: "scroll",
-    listStyleType: "none"
+    listStyleType: "none",
+    overflow: "scroll"
+
 };
 
-const STYLE_LI_FILE = {
-    padding: "6px",
-    borderBottom: "1px solid #ccc",
-    margin: "10px",
-    fontSize: "18px",
-};
 
-const STYLE_LI_FOLDER = {
-    padding: "6px",
-    borderBottom: "1px solid #999",
-    fontWeight: 400,
-    margin: "10px",
-    fontSize: "18px",
-};
-
-const STYLE_LI_ICON = {
-    float: "left",
-    margin: "0 15px 0 0"
-};
-
-const STYLE_LI_TIME = {
-    fontSize: "16px",
-    float: "right",
-    margin: "0 0 15px 0",
-    color: "#777"
-};
 
 
 export default class FileNavigation extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: this.props.text};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        this.props.saveCommentText(this.props.activeComment, this.state.value);
-        event.preventDefault();
     }
 
     render() {
+        let lis;
         return (
             <div style={{... STYLE}}>
                 <div style={{... STYLE_CENTER}}>
 
 
                     <ul style={{... STYLE_UL}}>
-                        <li style={{... STYLE_LI_FILE}}>
-                            <IconFile style={{... STYLE_LI_ICON}}/>
-                            Test test test
-                            <time style={{... STYLE_LI_TIME}}> 20/01/2017 24:00</time>
-                        </li>
-                        <li style={{... STYLE_LI_FILE}}>
-                            <IconFile style={{... STYLE_LI_ICON}}/>
-                            Test test test
-                            <time style={{... STYLE_LI_TIME}}> 20/01/2017 24:00</time>
-                        </li>
-                        <li style={{... STYLE_LI_FILE}}>
-                            <IconFile style={{... STYLE_LI_ICON}}/>
-                            Test test test
-                            <time style={{... STYLE_LI_TIME}}> 20/01/2017 24:00</time>
-                        </li>
-                        <li style={{... STYLE_LI_FILE}}>
-                            <IconFile style={{... STYLE_LI_ICON}}/>
-                            Test test test
-                            <time style={{... STYLE_LI_TIME}}> 20/01/2017 24:00</time>
-                        </li>
-                        <li style={{... STYLE_LI_FOLDER}}>
-                            <IconFolder style={{... STYLE_LI_ICON}}/>
-                            Test test test
-                            <time style={{... STYLE_LI_TIME}}> 20/01/2017 24:00</time>
-                        </li>
-
-
+                        {lis}
                     </ul>
 
                     <div style={{display: "inline-flex"}}>
                         <div style={{width: "100px", textAlign: "center"}}>
-                            <a
-                                href="javascript:;"
-                                style={{... STYLE_BUTTON}}
-                                title={"Salva"}
-                                onClick={this.handleSubmit}
-                            >
-                                <IconCheck style={{marginTop: "22px"}}/>
-                            </a>
+
                             <a
                                 href="javascript:;"
                                 style={{... STYLE_BUTTON}}
                                 title={"Annulla"}
-                                onClick={event => this.props.cancelModifyCommentTextFn()}
+                                onClick={event => this.props.cancelOpenItem()}
                             >
                                 <IconClose style={{marginTop: "22px"}}/>
                             </a>
@@ -167,8 +97,7 @@ export default class FileNavigation extends React.Component {
 }
 
 FileNavigation.propTypes = {
-    // text: PropTypes.string.isRequired,
-    // activeComment: PropTypes.number.isRequired,
-    // saveCommentText: PropTypes.func.isRequired,
-    // cancelModifyCommentTextFn: PropTypes.func.isRequired
+    // openItem: PropTypes.number.isRequired,
+    // loadSomething: PropTypes.func.isRequired
+    cancelOpenItem: PropTypes.func.isRequired
 };
